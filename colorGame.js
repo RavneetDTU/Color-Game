192f270225c89ac266ba5fb2f9f7ff3cd82390ba
Ravneet Punia <ravneet.dtu@Gmail.com>
2017-12-30 07:11:35 +0530

    Project complete in terms of functions and working

--- colorGame.js
+++ colorGame.js
@@ -1,19 +1,68 @@
 // Firstly we will make a list of Color......Initially we will make it same for all than we will randomize it
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 0, 255)",
-    "rgb(0, 255, 255)",
-    "rgb(255, 0, 255)",
-    "rgb(255, 255, 0)",
-];
-
+var numberOfSquares = 6;
+var colors = generateRandomColor(numberOfSquares);
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
-var colordisplay = document.getElementById("colordisplay")
+var pickedColor = pickrandomColor();
+var h1 = document.querySelector('h1');
+var colordisplay = document.getElementById("colordisplay");
 var messageDisplay = document.querySelector("#message");
+var resetButton = document.querySelector("#reset");
+var easybtn = document.querySelector("#easy");
+var hardbtn = document.querySelector("#hard");
 
-colordisplay.textContent = pickedColor;
+easybtn.addEventListener('click',function () {
+    hardbtn.classList.remove('selected');
+    easybtn.classList.add('selected');
+    numberOfSquares = 3;
+    // Generating 3 new Random Colors
+    colors = generateRandomColor(numberOfSquares);
+    // Picking a random color
+    pickedColor = pickrandomColor();
+    colordisplay.textContent = pickedColor.toUpperCase();
+    // Changing Color of top 3 Elements
+    for(var i = 0;i<squares.length;i++){
+        if(colors[i]){
+            squares[i].style.background = colors[i];
+        }
+        else {
+            // Don't make 3 Blocks to be Black as thay will be Still Clickable. Just Block them.
+            // squares[i].style.background = "#232323";
+            squares[i].style.display = "none";
+            // But when we click ResetButton, It Creates 6 Colors. And PickedColor can be from there
+        }
+    }
+});
+
+hardbtn.addEventListener('click',function () {
+    hardbtn.classList.add('selected');
+    easybtn.classList.remove('selected');
+    numberOfSquares = 6;
+    colors = generateRandomColor(numberOfSquares);
+    pickedColor = pickrandomColor();
+    colordisplay.textContent = pickedColor;
+    for(var i = 0; i<squares.length;i++){
+        squares[i].style.background = colors[i];
+        squares[i].style.display = "block";
+    }
+});
+
+resetButton.addEventListener('click',function () {
+   //Generate New Colors
+    colors = generateRandomColor(numberOfSquares);
+    // Pick a new random Color from Array
+    pickedColor = pickrandomColor();
+    // change colordisplay to match picked Color
+    colordisplay.textContent = pickedColor.toUpperCase();
+    //change color of Squares
+    for(var i = 0;i<squares.length;i++){
+        squares[i].style.background = colors[i];
+    }
+    // Change Color of H1 Background
+    h1.style.background = "#232323";
+
+});
+
+colordisplay.textContent = pickedColor.toUpperCase();
 
 for(i=0;i<squares.length;i++){
     // Adding Initial Colors to Squares
@@ -28,6 +77,8 @@ for(i=0;i<squares.length;i++){
         if(pickedColor === clickedColor){
             messageDisplay.textContent = "Correct !!";
             changeColor(clickedColor);
+            resetButton.textContent = "Play Again?"
+            h1.style.background = pickedColor;
 
         }else {
             this.style.background = "#232323";
@@ -36,6 +87,7 @@ for(i=0;i<squares.length;i++){
     });
 }
 
+
 function changeColor(color) {
     // Loop through all Squares
     for(var i = 0; i<colors.length;i++){
@@ -43,4 +95,21 @@ function changeColor(color) {
         squares[i].style.background = color;
     }
 
+}function pickrandomColor() {
+    // By Using Math.random() we can pick any random Color but it can returns a long decimal (But always less than 1) So we multiply it with 6
+    var random = Math.floor(Math.random()*colors.length);
+    return colors[random];
 }
+function generateRandomColor(num) {
+    // make an array
+    var arr = [];
+    // add num random colors to array
+    for (var i = 0;i<num;i++){
+        arr[i] = "rgb("+Math.floor(Math.random()*255)+", "+Math.floor(Math.random()*255)+", "+Math.floor(Math.random()*255)+")";
+        // For good practise make 3 Functions for red,green,blue and than Combine
+    }
+    // return that array
+    return arr;
+
+}
+
